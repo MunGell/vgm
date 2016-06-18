5d77968260cd8bce010f1670ef0c41868530a40a
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2016-06-18 18:16:59 +0100

    Add box-related command

--- src/box.js
+++ src/box.js
@@ -1,3 +1,19 @@
+const spawn = require('child_process').spawn;
+const configstore = require('./configstore');
+
 module.exports = function (box, cmd) {
-    console.log('box commands function', box, cmd);
-}
+    process.stdin.pause();
+    process.stdin.setRawMode(false);
+
+    var options = {
+            "cwd": configstore.get(box),
+            'stdio': 'inherit'
+        },
+        proc = spawn('vagrant', [cmd], options);
+
+    return proc.on('exit', function () {
+        process.stdin.setRawMode();
+        process.stdin.resume();
+        return process.exit();
+    });
+};
