6200bb997b2caffe81e7c203c699e2c1bc4cda37
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2016-06-19 21:42:29 +0100

    Add colours to cli output

--- src/add.js
+++ src/add.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const configstore = require('./configstore');
 const pathUtils = require('./path');
 
@@ -5,15 +6,15 @@ module.exports = function (name, vagrantpath) {
     vagrantpath = pathUtils.getPathOnly(vagrantpath);
 
     if (configstore.has(name)) {
-        console.error('Box with name %s is already registered in %s', name, configstore.get(name));
+        console.error(chalk.red('Box with name %s is already registered in %s'), name, configstore.get(name));
     } else if (!pathUtils.hasVagrantSetup(vagrantpath)) {
-        console.error('There is no Vagrant setup in %s', vagrantpath);
+        console.error(chalk.red('There is no Vagrant setup in %s'), vagrantpath);
     } else {
         configstore.set(name, vagrantpath);
         if (configstore.has(name)) {
-            console.info('Box called \'%s\' was successfully added to Vagrant Manager', name);
+            console.info(chalk.green('Box called \'%s\' was successfully added to Vagrant Manager'), name);
         } else {
-            console.error('We were not able to save the configuration file')
+            console.error(chalk.green('We were not able to save the configuration file'));
         }
     }
 };
--- src/remove.js
+++ src/remove.js
@@ -1,14 +1,15 @@
+const chalk = require('chalk');
 const configstore = require('./configstore');
 
 module.exports = function (box) {
     if (!configstore.has(box)) {
-        console.error('This box is not registered with Vagrant Manager');
+        console.error(chalk.red('This box is not registered with Vagrant Manager'));
     } else {
         configstore.del(box);
         if (!configstore.has(box)) {
-            console.info('Box called \'%s\' was successfully removed from Vagrant Manager', box);
+            console.info(chalk.green('Box called \'%s\' was successfully removed from Vagrant Manager'), box);
         } else {
-            console.error('We were not able to save the configuration file')
+            console.error(chalk.red('We were not able to save the configuration file'));
         }
     }
 };
