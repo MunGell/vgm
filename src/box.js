b761f2aebd60b1b3bc7f0924369c5890d20a819c
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2016-06-20 11:19:09 +0100

    Fix windows TTY issue and rename box to name variable

--- src/box.js
+++ src/box.js
@@ -1,20 +1,25 @@
 const spawn = require('child_process').spawn;
 const configstore = require('./configstore');
 
-module.exports = function (box, cmd) {
+module.exports = function (name, cmd) {
     cmd = cmd || 'status';
 
     process.stdin.pause();
-    process.stdin.setRawMode(false);
+
+    if (process.stdin.isTTY) {
+        process.stdin.setRawMode(false);
+    }
 
     var options = {
-            "cwd": configstore.get(box),
+            "cwd": configstore.get(name),
             'stdio': 'inherit'
         },
         proc = spawn('vagrant', [cmd], options);
 
     return proc.on('exit', function () {
-        process.stdin.setRawMode();
+        if (process.stdin.isTTY) {
+            process.stdin.setRawMode();
+        }
         process.stdin.resume();
         return process.exit();
     });
